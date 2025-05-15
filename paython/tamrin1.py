21b72a83016af0b99b1572c59c2df1448a799667
iyasink <i.yasiwn@gmail.com>
2025-05-16 00:05:05 +0330

    Delete paython directory

--- paython/tamrin1.py
+++
@@ -1,5 +0,0 @@
-number=input("enter the number:")
-number=int(number)
-while number>0:
-    print(number)
-    number-=1
--- paython/tamrin4.py
+++
@@ -1,7 +0,0 @@
-#عددی رو دریافت کرده و اعداد زوج کوچکتر از آن را چاپ کند
-number=input("enter the number:")
-number=int(number)
-while number>=0:
-    if number%2==0:
-        print(number)
-    number-=1
